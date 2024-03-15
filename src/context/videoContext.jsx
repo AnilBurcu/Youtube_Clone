bad6138110149cd6852652089700d84008aee9ab
AnilBurcu <anlbrc95@gmail.com>
2024-03-15 14:23:46 +0300

    API Request

--- src/context/videoContext.jsx
+++ src/context/videoContext.jsx
@@ -1,10 +1,17 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
+import { categories } from "../constants";
+import { getData } from "../helpers/getData";
 // 1) context temelini oluştur
 export const VideoContext = createContext();
 // 2) Sağlayıcı tanımla
 export const VideoProvider = ({ children }) => {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
+  // kategori her değiştiğinde api'den veriyi al
+  useEffect(() => {
+    getData("/home").then((data) => console.log(data));
+  }, [selectedCategory]);
   return (
     <VideoContext.Provider value={{ selectedCategory, setSelectedCategory }}>
       {children}
