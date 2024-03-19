ba95123b0cd064ee1fd399660c0d1de584370014
AnilBurcu <anlbrc95@gmail.com>
2024-03-19 18:16:16 +0300

    search results

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Feed from "./pages/Feed";
 import Header from "./components/Header";
 import VideoDetail from "./pages/VideoDetail";
+import SearchResult from "./pages/SearchResult";
+import Undefined from "./pages/Undefined";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/watch" element={<VideoDetail />} />
+        <Route path="/results" element={<SearchResult />} />
+        <Route path="*" element={<Undefined />} />
       </Routes>
     </BrowserRouter>
   );
